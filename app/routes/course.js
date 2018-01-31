e4c0f771a599e9b85a17338f235b027e57107a77
Soo Hwan Park <sopark@linkedin.com>
2018-01-30 16:14:43 -0800

    Mike solution

--- app/routes/course.js
+++ app/routes/course.js
@@ -3,10 +3,12 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model(params) {
     let { id: slug } = params;
-    const courses = this.modelFor('application').data.map(course => {
-      return course.attributes;
-    });
+    // const courses = this.modelFor('application').data.map(course => {
+    //   return course.attributes;
+    // });
+
     // debugger;
+    const courses = this.modelFor('application');
     return courses.filter(course => course.slug === slug)[0];
     // return this.modelFor('application').filter(
     //   course => course.title === title
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,9 +2,10 @@ import Route from '@ember/routing/route';
 
 export default Route.extend ({
   model () {
-    const courses = this.modelFor ('application').data.map (course => {
-      return course.attributes;
-    });
+    // const courses = this.modelFor ('application').data.map (course => {
+    //   return course.attributes;
+    // });
+    const courses = this.modelFor('application');
     console.log ('courses', courses);
     return courses;
   },
